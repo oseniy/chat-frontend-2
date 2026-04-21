0aa1a6cb7e2a4bc54763ba33f66251b68c3b4be1
oseniy <ars7080947@gmail.com>
2026-04-21 18:11:21 +0300

    fix: set-50 для регистрации

--- src/features/auth/userForm/model/validation.ts
+++ src/features/auth/userForm/model/validation.ts
@@ -26,7 +26,6 @@ const firstNameSchema = z
 const nicknameSchema = z
   .string()
   .min(1, "Заполните поле")
-  .min(5, "Не менее 5 символов")
   .max(32, "Не более 32 символов")
   .regex(/^[A-Za-z0-9_-]+$/, {
     message: "Допустимы: латиница, цифры, дефис (-) и подчёркивание (_)",
@@ -39,7 +38,8 @@ const nicknameSchema = z
   })
   .refine((val) => !/--|__/.test(val), {
     message: "Нельзя использовать -- или __ подряд",
-  });
+  })
+  .min(5, "Не менее 5 символов");
 
 const userFormSchema = z.object({
   firstName: firstNameSchema,
